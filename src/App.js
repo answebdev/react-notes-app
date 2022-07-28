761985c3ebfe7a2e0bf51da5e228a714a2cec59f
ANS <36783010+answebdev@users.noreply.github.com>
2022-07-28 15:24:50 -0700

    add react icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Video: https://www.youtube.com/watch?v=8KB3DHI-QbM
+// Code: https://github.com/chrisblakely01/react-notes-app
+// CSS: https://raw.githubusercontent.com/chrisblakely01/react-notes-app/master/src/index.css
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
