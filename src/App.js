63854dd25bf001834ddd58ac86f930e3629e3a5f
ANS <36783010+answebdev@users.noreply.github.com>
2022-07-28 19:25:01 -0700

    add note data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList';
 // Video: https://www.youtube.com/watch?v=8KB3DHI-QbM
 // Code: https://github.com/chrisblakely01/react-notes-app
 // CSS: https://raw.githubusercontent.com/chrisblakely01/react-notes-app/master/src/index.css
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note!',
+      date: '07/28/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my second note!',
+      date: '07/29/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my third note!',
+      date: '08/02/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my fourth note!',
+      date: '08/05/2022',
+    },
+  ]);
+
   return (
     <div className='container'>
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
