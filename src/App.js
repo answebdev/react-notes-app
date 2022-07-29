83a79a579c34a10ea45ec7eebadafec5acba85a5
ANS <36783010+answebdev@users.noreply.github.com>
2022-07-28 18:59:07 -0700

    add shell components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import NotesList from './components/NotesList';
 // Video: https://www.youtube.com/watch?v=8KB3DHI-QbM
 // Code: https://github.com/chrisblakely01/react-notes-app
 // CSS: https://raw.githubusercontent.com/chrisblakely01/react-notes-app/master/src/index.css
 
 const App = () => {
   return (
-    <div>
-      <p>React Notes Application</p>
+    <div className='container'>
+      <NotesList />
     </div>
   );
 };
